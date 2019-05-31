43f8715a1029009dbe69112257ceb29afb3610ff
stamp <>
2019-05-31 11:22:36 +0700

    Project

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -1,30 +1,60 @@
 /*
  * WIP XI Computer Programing 
  */
-
 package XI.Project;
+
+import java.util.Scanner;
 
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
+
     public static void main(String[] args) {
-        int age = 18;
-        String name = "Wippo";
-        double money = 1100.11;
-        
-        boolean online = true;
-        String status;
-        
-        System.out.println("Hi! my name is " + name + " i'm " + age + " Y/O ");
-        
-        if(online == true){
-            status = "Online";
-        } else {
-            status = "Offline";
+        Scanner sc = new Scanner(System.in);
+        int item = 0;
+        int limit = 10;
+        int menu = 0;
+        int amount=0;
+        while (true) {
+            System.out.println("1.Add\n2.Remove\n3.Check\n");
+            System.out.print("menu : ");
+            menu = sc.nextInt();
+
+            if (menu == 1) {
+                if (item == limit) {
+                    System.out.println("ไอเท็มเต็ฒแล้ว");
+                } else {
+                    System.out.print("Amount : ");
+                    amount = sc.nextInt();
+                }
+                if (item + amount > limit) {
+                    System.out.println("ไอเท็มเกินลิมิต");
+                } else {
+                    item += amount;
+                    System.out.println("Item : " + item);
+                    System.out.println("Limit : " + limit);
+                }
+            } else if (menu == 2) {
+                if (item == 0) {
+                    System.out.println("ไม่มีไอเท็มในโกดัง");
+                } else {
+                    amount = sc.nextInt();
+                }
+                if (item - amount > limit) {
+                    System.out.println("ไอเท็มไม่เพียงพอ");
+                } else {
+                    item -= amount;
+                    System.out.println("Item : " + item);
+                    System.out.println("Limit : " + limit);
+                }
+            } else {
+                System.out.println("Item : " + item);
+                System.out.println("Limit : " + limit);
+            }
+            // System.out.println("Limit : " + limit);
+            System.out.println("----------------------------");
         }
-        
-        System.out.println("I'm currently " + status + " and have " + money + "THB. left.");
     }
 }
